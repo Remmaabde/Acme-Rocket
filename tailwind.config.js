fed1a801c5eb0138fb72ec0276653997f4eefb9e
Reema <Reemabdella702@gmail.com>
2025-06-22 11:17:19 +0300

    Modified Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./build/*.html'], // ✅ Tells Tailwind where to look not only index but to look for all html files inside build folder
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
   },
   plugins: [],
 }
